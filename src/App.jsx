bba34c536e35ff4489806d94c9440e353f8200b3
Denyse-Ishimirwe <ishimirwedenyse@gmail.com>
2026-03-11 15:55:34 +0200

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <><div>
+    <div className= "login-container">
       <h1>QA HELPER</h1>
-      <input type="email" placeholder="Enter your email address"></input>
-      <input type="password" placeholder="Enter your password"></input>
-       <button> login</button></>
+      <input type= "email" placeholder= "Enter your email address"></input>
+      <input type= "password" placeholder= "Enter your password"></input>
+      <button> login</button>
     </div>
   )
 }
