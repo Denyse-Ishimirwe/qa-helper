ea06fa2abbaaeca8a3ad033cf6432a87088b81bd
Denyse-Ishimirwe <ishimirwedenyse@gmail.com>
2026-03-11 12:43:55 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 
 function App() {
   return (
-    <div>
+    <><div>
       <h1>QA HELPER</h1>
-      <input type= "email" placeholder= "Enter your email address"></input>
-      <input type= "password" placeholder= "Enter your password"></input>
-      <button> login</button>
+      <input type="email" placeholder="Enter your email address"></input>
+      <input type="password" placeholder="Enter your password"></input>
+       <button> login</button></>
     </div>
   )
 }
